90c2da0213fca96f30d709c7bd28e6df193233eb
Mediocrych <148034597+Mdcrych@users.noreply.github.com>
2024-11-06 23:17:26 +0500

    Update and rename lab2_2 to lab2_2.cpp

---
+++ lab2_2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <iterator>
+#include <list>
+
+int input(int a, std::string str = "Введите размер контейнера: ") {
+  std::cout << str;
+  std::cin >> a;
+  return a;
+}
+
+int main() {
+    std::list<int> L;
+    short unsigned int r, m;
+    r = input(r,"Введите кол-во элементов вектора ");
+    if ( r%3 !=0 ){
+        std::cout << "число элементов не кратно 3";
+        return -1;}
+    else{
+    for (int i {0}; i < r; i++){
+        m = input(m,"Введите элемент вектора ");
+        L.push_back(m);}}
+    int j = 1;
+    j=(L.size()+1)/3-1;
+    std::cout << j<< std::endl;
+    std::list<int>::iterator it = L.begin();
+    std::advance(it, j);
+    while(it !=L.begin()) {
+        L.push_back(*it);
+        it--;
+    }
+    L.push_back(*it);
+    std::copy(L.begin(), L.end(), std::ostream_iterator<int>(std::cout, " "));
+}
