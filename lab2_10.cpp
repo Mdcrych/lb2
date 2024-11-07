62e7b5b9207eaee4ef7ebd765420223ec3c7e857
Mediocrych <148034597+Mdcrych@users.noreply.github.com>
2024-11-07 23:53:23 +0500

    Update lab2_10.cpp

--- lab2_10.cpp
+++ lab2_10.cpp
@@ -5,17 +5,25 @@
 
 int main() {
     setlocale(LC_ALL, "Russian");
-    std::vector<std::string> V = {"APPLE", "ORANGE", "GRAPE", "LEMON", "PEAR", "PEACH"};
-
-    std::multimap<char, std::string> M;
-
-    for(auto it = V.rbegin(); it != V.rend(); ++it) {
-        M.insert(std::make_pair((*it)[(*it).size() - 1], *it));
-    }
-
-    for(const auto& pair : M) {
-        std::cout << pair.first << ": " << pair.second << std::endl;
-    }
-
-    return 0;
+    std::vector<std::string> V;
+    int n;
+    std::cout << "Введите количество слов: ";
+    std::cin >> n;
+    for (int i = 0; i < n; i++) {
+    std::string input;
+    std::cout << "Введите слово Caps Lock`ом: ";
+    std::cin >> input;
+    V.push_back(input);
 }
+
+std::multimap<char, std::string> M;
+
+for (auto it = V.rbegin(); it != V.rend(); ++it) {
+    M.insert(std::make_pair((*it)[(*it).size() - 1], *it));
+}
+
+for (const auto& pair : M) {
+    std::cout << pair.first << ": " << pair.second << std::endl;
+}
+
+return 0;}
