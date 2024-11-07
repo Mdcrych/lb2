7e9ff0f92e68f35a9adae1c4088ac34992a1e988
Mediocrych <148034597+Mdcrych@users.noreply.github.com>
2024-11-07 16:06:20 +0500

    Update lab2_7.cpp

--- lab2_7.cpp
+++ lab2_7.cpp
@@ -4,6 +4,7 @@
 #include <algorithm>
 
 int main() {
+setlocale(LC_ALL, "Russian");
 std::list<int> L = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
 auto isEven = [](int n) { return n % 2 == 0; };
@@ -13,8 +14,8 @@ auto it = std::partition(L.begin(), L.end(), isEven);
 int numEven = std::distance(L.begin(), it);
 int numOdd = std::distance(it, L.end());
 
-std::cout << "Number of even numbers: " << numEven << std::endl;
-std::cout << "Number of odd numbers: " << numOdd << std::endl;
+std::cout << "четные: " << numEven << std::endl;
+std::cout << "нечетные: " << numOdd << std::endl;
 
 return 0;
 }
