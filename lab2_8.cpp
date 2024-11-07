a310aadaa9b76548fbe363bc931fdd53b087b0b4
Mediocrych <148034597+Mdcrych@users.noreply.github.com>
2024-11-07 21:22:48 +0500

    Update lab2_8.cpp

--- lab2_8.cpp
+++ lab2_8.cpp
@@ -1,19 +1,29 @@
 #include <iostream>
 #include <vector>
+#include <iterator>
 #include <algorithm>
 #include <numeric>
 
+int input(int a, std::string str = "Введите размер контейнера: ") {
+  std::cout << str;
+  std::cin >> a;
+  return a;
+}
 class AverageCalculator {
 public:
     double operator()(int a, int b) const {
         return (a + b) / 2.0;
     }
 };
-
 int main() {
-    setlocale(LC_ALL, "Russian");
-    std::vector<int> L = {1, 3, 4, 6};
-    std::vector<double> V;
+setlocale(LC_ALL, "Russian");
+std::vector<int>  L;
+short unsigned int l, n;
+    l = input(l,"Введите кол-во элементов списка ");
+    for (int i {0}; i < l; i++){
+        n = input(n,"Введите элемент списка ");
+        L.push_back(n);}
+std::vector<double> V;
 
     std::adjacent_difference(L.begin(), L.end(), std::back_inserter(V), AverageCalculator());
 
@@ -26,4 +36,3 @@ int main() {
 
     return 0;
 }
-
