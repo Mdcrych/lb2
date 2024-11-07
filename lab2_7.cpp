6f1498d48cea53087dbf5184971db04010a10f42
Mediocrych <148034597+Mdcrych@users.noreply.github.com>
2024-11-07 21:09:32 +0500

    Update lab2_7.cpp

--- lab2_7.cpp
+++ lab2_7.cpp
@@ -3,10 +3,19 @@
 #include <iterator>
 #include <algorithm>
 
+int input(int a, std::string str = "Введите размер контейнера: ") {
+  std::cout << str;
+  std::cin >> a;
+  return a;
+}
 int main() {
 setlocale(LC_ALL, "Russian");
-std::list<int> L = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-
+std::list<int> L;
+short unsigned int l, n;
+    l = input(l,"Введите кол-во элементов списка ");
+    for (int i {0}; i < l; i++){
+        n = input(n,"Введите элемент списка ");
+        L.push_back(n);}
 auto isEven = [](int n) { return n % 2 == 0; };
 
 auto it = std::partition(L.begin(), L.end(), isEven);
