fb4f927cee0c1891b29ba19dc6ed53b56921d8f3
Mediocrych <148034597+Mdcrych@users.noreply.github.com>
2024-11-07 16:35:29 +0500

    Create lab2_9.cpp

---
+++ lab2_9.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <set>
+
+int main() {
+std::vector<int>V0 = {1, 2, 3};
+int N = 3;
+std::vector<std::vector<int>> V = {
+{1, 2, 3, 4},
+{1, 2, 3},
+{2, 3, 4}
+};
+int count = 0;
+std::multiset<int> sV0(V0.begin(), V0.end());
+for (int i = 0; i < N; i++) {
+    std::multiset<int> sVi(V[i].begin(), V[i].end());
+    if (std::includes(sVi.begin(), sVi.end(), sV0.begin(), sV0.end())) {
+        count++;
+    }
+}
+std::cout << "Number of vectors containing all elements of V0: " << count << std::endl;
+return 0;
+}
