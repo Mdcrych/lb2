aea5e2650d15577a8e45cc8bf31233d7e6a40a9b
Mediocrych <148034597+Mdcrych@users.noreply.github.com>
2024-11-07 16:55:54 +0500

    Create lab2_10.cpp

---
+++ lab2_10.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+int main() {
+    std::vector<std::string> V = {"APPLE", "ORANGE", "GRAPE", "LEMON", "PEAR", "PEACH"};
+
+    std::multimap<char, std::string> M;
+
+    for(auto it = V.rbegin(); it != V.rend(); ++it) {
+        M.insert(std::make_pair((*it)[(*it).size() - 1], *it));
+    }
+
+    for(const auto& pair : M) {
+        std::cout << pair.first << ": " << pair.second << std::endl;
+    }
+
+    return 0;
+}
